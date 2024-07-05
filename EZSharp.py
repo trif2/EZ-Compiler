67e81e8298b044d5af64b74b89678afebeae42ea
Zachary Wardley <zachary.wardley@gmail.com>
2024-07-05 16:59:56 -0400

    Correct parameter file paths

--- EZSharp.py
+++ EZSharp.py
@@ -5,21 +5,18 @@ from code_generation import *
 file = "testing/Test6.cp"
 
 # Transition Table
-table = get_table("TTFinal.txt")
+table = get_table("parameters/TTFinal.txt")
 
 # LL1 Table
-ll_table = get_table("ll1.txt")
+ll_table = get_table("parameters/ll1.txt")
 
 # Grammar
-grammar = get_grammar("grammar.txt")
+grammar = get_grammar("parameters/grammar.txt")
 
-def compile(file_name):
+def compile(file_name, table, ll_table, grammar):
     # Check that file extension is correct (.cp)
     if file_name[-3:] != ".cp":
         return
-    
-    # Parameter for lexical_analysis()
-    table = get_table("TTFinal.txt")
     
     print("Lexical now")
     tokens = lexical_analysis(file_name, table)
@@ -28,10 +25,6 @@ def compile(file_name):
     for token in tokens:
         token_file.write(f"{token[0]} {token[1]}\n")
     token_file.close()
-    
-    # Parameters for syntax_analysis(), semantic_analysis(), and generate_code()
-    ll_table = get_table("ll1.txt")
-    grammar = get_grammar("grammar.txt")
     
     print("Syntax now")
     tokens = syntax_analysis(file_name, tokens, grammar, ll_table)
@@ -44,13 +37,16 @@ def compile(file_name):
     for token in tokens:
         token_file.write(f"{token[0]} {token[1]}\n")
     token_file.close()
+    
     print("Semantic now")
     if not semantic_analysis(file_name, tokens, grammar, ll_table):
         return 1
+    
     print("Code generation now")
     if not generate_code(file_name, tokens, grammar, ll_table):
         return 1
+    
     print("Literally nothing wrong") # Prints if no errors :D
     return 0
 
-compile(file)
+compile(file, table, ll_table, grammar)
